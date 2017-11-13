3ed55060a5eea4a7fb2b6f111e6db221c42fe553
Jackson Huang <jackson_huang@live.com>
2017-11-13 21:58:03 +1100

    Basic UI with image and header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import img from "./checkmate.jpg"
+import "./App.css";
+
+class Instruction extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    console.log(this.props.text);
+    return <p>{this.props.text}</p>;
+  }
+}
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  header() {
+    return (
+      <div className="Header">
+        <Instruction text={this.props.instruction} />
+      </div>
+    );
+  }
+
+  render() {
+    return this.header();
+  }
+}
+
+class SourceImage extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <img className="ImageToLabel" src={this.props.imageUrl} alt="" />
+    );
+  }
+}
+
+class LabelView extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div>
+        <SourceImage imageUrl={this.props.imageUrl} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header instruction="Draw bounding boxes around every chess piece." />
+        <LabelView imageUrl={img} />
       </div>
     );
   }
