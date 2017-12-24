3472c3ffb9195ca44da47cac31c071e811f2536e
Jackson Huang <jackson_huang@live.com>
2017-12-24 14:08:38 +1100

    Normalize box and remove old form

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -11,6 +11,8 @@ export default class SubmitButton extends Component {
     this.props = props;
     this.submitTask = this.submitTask.bind(this);
     this.getSubmissionUrl = this.getSubmissionUrl.bind(this);
+    this.getNormalizedBoxes = this.getNormalizedBoxes.bind(this);
+    this.normalizePosition = this.normalizePosition.bind(this);
   }
 
   hasAcceptedTask(search) {
@@ -18,20 +20,42 @@ export default class SubmitButton extends Component {
     return parsed.assignmentId !== "ASSIGNMENT_ID_NOT_AVAILABLE";
   }
 
-  normalizeBoxPosition(position) {
+  /*
+   * Return an Array of normalized box positions. (no id)
+   */
+  getNormalizedBoxes() {
+    const normalizedBoxes = [];
+    for (var key in this.props.boundingBoxes) {
+      const box = this.props.boundingBoxes[key].position;
+      const normalizedBox = this.normalizePosition(box);
+      normalizedBoxes.push(normalizedBox);
+    }
+    return normalizedBoxes;
+  }
 
+  normalizePosition(position) {
+    const { top, left, width, height } = position;
+    console.log(top, left, width, height);
+    const normalizedPosition = {
+      top: top / this.props.imageHeight,
+      left: left / this.props.imageWidth,
+      width: width / this.props.imageWidth,
+      height: height / this.props.imageHeight
+    };
+    // round to 2 decimal places
+    for (var key in normalizedPosition) {
+      normalizedPosition[key] = normalizedPosition[key].toFixed(2);
+    }
+    return normalizedPosition;
   }
 
   submitTask(e) {
     const parsed = queryString.parse(this.props.location.search);
-    e.preventDefault();
+    // e.preventDefault();
     console.log('POSTing data');
     axios
-      .post(`${config["server"][env]}/boxes/${this.props.taskId}`, {
-        boundingBoxes: this.props.boundingBoxes,
-        assignmentId: parsed.assignmentId,
-        workerId: parsed.workerId,
-        hitId: parsed.hitId
+      .post(`${this.getSubmissionUrl()}`, {
+ 
       })
       .then(function(response) {
         console.log(response);
@@ -44,39 +68,32 @@ export default class SubmitButton extends Component {
   createInputElement() {
     if (this.hasAcceptedTask(this.props.location.search)) {
       if (this.props.hasDrawnBox) {
-        const value = "Submit";
+        var value = "Submit";
         var inputElement = <input type="submit" id="submitButton" value={value} />;
       } else {
-        const value = "Draw a box first!"
+        var value = "Draw a box first!"
         var inputElement = <input type="submit" id="submitButton" value={value} disabled />
       }
     } else {
-      const value = "You must ACCEPT the HIT before you can submit the results.";
+      var value = "You must ACCEPT the HIT before you can submit the results.";
       var inputElement = <input type="submit" id="submitButton" value={value} disabled />;
     }
-    return inputElement;
+    return value;
   }
 
   getSubmissionUrl() {
-    const normalizedBoundingBoxes = this.normalizeBoxPosition();
-
-    return config["submit"][env] + qs.stringify(this.props.boundingBoxes, { addQueryPrefix: true, arrayLimit: 0 });
+    return config["submit"][env] + qs.stringify(
+      this.getNormalizedBoxes(),
+      { addQueryPrefix: true });
   }
 
   render() {
-    console.log('h,w: ', this.props.imageHeight, this.props.imageWidth);
     const submissionUrl = this.getSubmissionUrl();
     const inputElement = this.createInputElement();
 
     return (
-      <div id="Submit">
-        <div id="SubmissionForm">
-          <form name="mturkForm" method="post" action={submissionUrl}>
-          
-          {/*<form name="mturkForm">*/}
-            {inputElement}
-          </form>
-        </div>
+      <div id="submitButton2" onClick={this.submitTask}>
+        {inputElement}
       </div>
     );
   }
