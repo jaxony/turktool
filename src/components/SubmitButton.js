d4b4516dc2ddf945e80fbabf0facd8b85f97af90
Jackson Huang <jackson_huang@live.com>
2017-12-24 13:26:01 +1100

    Added qs library

--- src/components/SubmitButton.js
+++ src/components/SubmitButton.js
@@ -3,11 +3,14 @@ import axios from 'axios';
 var env = process.env.NODE_ENV;
 var config = require("../config.json");
 const queryString = require('query-string');
+var qs = require('qs');
 
 export default class SubmitButton extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.submitTask = this.submitTask.bind(this);
+    this.getSubmissionUrl = this.getSubmissionUrl.bind(this);
   }
 
   hasAcceptedTask(search) {
@@ -15,9 +18,14 @@ export default class SubmitButton extends Component {
     return parsed.assignmentId !== "ASSIGNMENT_ID_NOT_AVAILABLE";
   }
 
+  normalizeBoxPosition(position) {
+
+  }
+
   submitTask(e) {
     const parsed = queryString.parse(this.props.location.search);
     e.preventDefault();
+    console.log('POSTing data');
     axios
       .post(`${config["server"][env]}/boxes/${this.props.taskId}`, {
         boundingBoxes: this.props.boundingBoxes,
@@ -37,7 +45,7 @@ export default class SubmitButton extends Component {
     if (this.hasAcceptedTask(this.props.location.search)) {
       if (this.props.hasDrawnBox) {
         const value = "Submit";
-        var inputElement = <input type="submit" id="submitButton" value={value} onClick={this.props.submitTask} />;
+        var inputElement = <input type="submit" id="submitButton" value={value} />;
       } else {
         const value = "Draw a box first!"
         var inputElement = <input type="submit" id="submitButton" value={value} disabled />
@@ -49,14 +57,22 @@ export default class SubmitButton extends Component {
     return inputElement;
   }
 
+  getSubmissionUrl() {
+    const normalizedBoundingBoxes = this.normalizeBoxPosition();
+
+    return config["submit"][env] + qs.stringify(this.props.boundingBoxes, { addQueryPrefix: true, arrayLimit: 0 });
+  }
+
   render() {
-    const submissionUrl = config["submit"][env];
+    console.log('h,w: ', this.props.imageHeight, this.props.imageWidth);
+    const submissionUrl = this.getSubmissionUrl();
     const inputElement = this.createInputElement();
 
     return (
       <div id="Submit">
-        <div>
+        <div id="SubmissionForm">
           <form name="mturkForm" method="post" action={submissionUrl}>
+          
           {/*<form name="mturkForm">*/}
             {inputElement}
           </form>
--- src/containers/SubmitButtonContainer.js
+++ src/containers/SubmitButtonContainer.js
@@ -12,7 +12,9 @@ const mapStateToProps = (state, ownProps) => {
   return {
     hasDrawnBox: Object.keys(committedBoxes).length > 0,
     boundingBoxes: committedBoxes,
-    taskId: ownProps.taskId
+    taskId: ownProps.taskId,
+    imageHeight: state.imageProps.height,
+    imageWidth: state.imageProps.width
   };
 };
 
