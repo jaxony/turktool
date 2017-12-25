b4820a480cfab03e2fab079da5188956d1de2f7e
Jackson Huang <jackson_huang@live.com>
2017-12-26 03:13:26 +1100

    Delete button only shows when cursor in box

--- src/components/BoundingBox.js
+++ src/components/BoundingBox.js
@@ -5,13 +5,31 @@ export default class BoundingBox extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.mouseOverHandler = this.mouseOverHandler.bind(this);
+    this.mouseLeaveHandler = this.mouseLeaveHandler.bind(this);
+    this.state = {
+      mouseOver: false
+    };
+  }
+
+  mouseOverHandler() {
+    this.setState({ mouseOver: true });
+  }
+
+  mouseLeaveHandler() {
+    this.setState({ mouseOver: false });
   }
 
   render() {
     // console.log(this.props.box.position);
     return (
-      <div className="BoundingBox" style={this.props.box.position}>
-        <DeleteBoxButtonContainer boxId={this.props.box.id} />
+      <div
+        className="BoundingBox"
+        style={this.props.box.position}
+        onMouseOver={this.mouseOverHandler}
+        onMouseLeave={this.mouseLeaveHandler}
+      >
+        {this.state.mouseOver && <DeleteBoxButtonContainer boxId={this.props.box.id} />}
       </div>
     );
   }
