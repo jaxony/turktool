5ca274099c597ef807dcd30369e7939ec2958e5c
Jackson Huang <jackson_huang@live.com>
2017-12-26 03:13:02 +1100

    Delete box working

--- src/components/DeleteBoxButton.js
+++ src/components/DeleteBoxButton.js
@@ -4,11 +4,17 @@ export default class DeleteBoxButton extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler() {
+    console.log("Delete box " + this.props.boxId);
+    this.props.deleteBox(this.props.boxId);
   }
 
   render() {
     return (
-      <div className="DeleteBoxButton">
+      <div className="DeleteBoxButton" onClick={this.clickHandler}>
         X
       </div>
     );
