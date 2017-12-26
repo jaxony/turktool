17ec92689fd00b46f863b6e78f94406f12b13ca9
Jackson Huang <jackson_huang@live.com>
2017-12-26 19:31:34 +1100

    Fix: limit lines

--- src/components/HorizontalLine.js
+++ src/components/HorizontalLine.js
@@ -4,12 +4,19 @@ export default class HorizontalLine extends Component {
   constructor(props) {
     super(props);
     this.props = props;
+    this.calculateTop = this.calculateTop.bind(this);
+    this.DIV_BORDER = 1;
+  }
+
+  calculateTop() {
+    const top = Math.max(0, this.props.y - this.props.imageProps.offsetY);
+    return Math.min(this.props.imageProps.height, top);
   }
 
   render() {
     return (
       <div id="horizontalLine" className="line" style={{
-        top: this.props.y - this.props.imageProps.offsetY,
+        top: this.calculateTop(),
         width: this.props.imageProps.width
       }}></div>
     );
