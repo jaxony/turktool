727848c71bdb385654bec498b089c6b4b87fea41
Jackson Huang <jackson_huang@live.com>
2018-01-22 10:24:21 +0800

    un-nest redux state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import committedBoxes from "./committedBoxes";
 import imageProps from "./imageProps";
-import main from "./main";
 
 const turktoolApp = combineReducers({
-  turktool: main
+  committedBoxes: committedBoxes,
+  imageProps: imageProps
 })
 
 export default turktoolApp;
